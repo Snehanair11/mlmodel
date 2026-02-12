afbbf66fa18e8a955686b4c3b3290316536fcf3d
Sneha S Nair <snehasnair1149@gmail.com>
2026-02-12 15:15:20 +0530

    Delete download_model.py

--- download_model.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-print("Pre-downloading emotion model...")
-pipeline(
-    "text-classification",
-    model="j-hartmann/emotion-english-distilroberta-base",
-    top_k=3
-)
-print("Model cached.")
